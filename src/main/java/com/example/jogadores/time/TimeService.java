4666b5d49a1d9a06f2b35759cdfe83f11e8bae19
Henrique Mayor <henriquessm@al.insper.edu.br>
2024-09-16 15:19:14 -0300

    ABC

---
+++ src/main/java/com/example/jogadores/time/TimeService.java
@@ -0,0 +1,17 @@
+package com.example.jogadores.time;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class TimeService {
+
+    public ResponseEntity<RetornarTImeDTO> getTime(String idTime) {
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.getForEntity(
+                "http://localhost:8080/tjime/" + idTime,
+                RetornarTImeDTO.class);
+    }
+}
